fc45c4f369ad69f8d74ce557f8211c68632ed57c
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-02-16 15:49:25 -0600

    stuck here

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ let val = 5 + 4
 const unitedStates = '50 nifty United States from 13 original colonies'
 
 // hello step 7 
-function sayHello() {
-    alert('Hello World ' + 'from the ' + unitedStates + '.');
-};
+// function sayHello() {
+//     alert('Hello World ' + 'from the ' + unitedStates + '.');
+// };
 
-sayHello();
+// sayHello();
 
 // you aren't old enough to view step 8 
 function checkAge(name, age) {
-    if(age < 21) {
-    alert('Sorry ' + name + ', you aren\'t old enough to view this page!');
-    }
-}
+    if (age < 21) {
+        alert('Sorry ' + name + ', you aren\'t old enough to view this page!');
+    };
+};
 
 checkAge('Charles', 21);
 checkAge('Abby', 27);
@@ -25,9 +25,31 @@ checkAge('James', 18);
 checkAge('John', 17);
 
 // step 9 veggies
-let veggies = ['carrots', 'zucchini', 'baby corn', 'green peppers', 'potatoes'];
+// let veggies = ['carrots', 'zucchini', 'baby corn', 'green peppers', 'potatoes'];
 
-for(var i = 0; i < veggies.length; i++) {
-    console.log(veggies[i]);
+// for (var i = 0; i < veggies.length; i++) {
+//     console.log(veggies[i]);
+// }
+
+// step 10 object
+let people = [{
+    name: 'Jim',
+    age: 25,
+}, {
+    name: 'Pam',
+    age: 21,
+}, {
+    name: 'Kelly',
+    age: 19,
+}, {
+    name: 'Michael',
+    age: 20,
+}, {
+    name: 'Creed',
+    age: 82,
+},
+];
+
+for (var i = 0; i < people.length; i++) {
+    checkAge(people);
 }
-
