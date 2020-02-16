11633f94ceae9373002cc9ad22452a58571d8217
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-02-16 13:05:30 -0600

    hello step 7

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // variables: name, US States count, 5 + 4
 let name = 'Liz'
-const unitedStates = '50 nifty United States from 13 original colonies'
 let val = 5 + 4
 
-// print current value or val
-console.log(unitedStates);
+// sorry this song is going to be stuck in your head all day :)
+const unitedStates = '50 nifty United States from 13 original colonies'
+
+
+function sayHello() {
+    console.log('Hello World ' + 'and ' + name + 'from the ' + unitedStates + '.');
+}
+
+sayHello();
