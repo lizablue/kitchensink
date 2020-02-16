f61f7891d6443e8d4393fb688c01967027fa47f3
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-02-16 12:53:38 -0600

    step 6, change const variable from step 4

---
+++ app.js
@@ -0,0 +1,7 @@
+// variables: name, US States count, 5 + 4
+let name = 'Liz'
+const unitedStates = '50 nifty United States from 13 original colonies'
+let val = 5 + 4
+
+// print current value or val
+console.log(unitedStates);
